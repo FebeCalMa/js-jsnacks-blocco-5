3aa0aa6fde291ba92b1490b73c1a7346c31e7d18
Febe Calderon <samcalma1@gmail.com>
2022-03-22 16:11:45 +0100

    arrahy con zucchine

---
+++ script.js
@@ -0,0 +1,61 @@
+console.log("JS OK!");
+
+/*
+Crea un array di 10 oggetti che rappresentano una zucchina, 
+indicando per ognuna varietà, peso e lunghezza.
+Calcola quanto pesano tutte le zucchine.
+*/
+
+//
+const zucchine = [
+  {
+    varieta: "Zucchina Nera",
+    peso: "200gr",
+    lunghezza: "3cm",
+  },
+  {
+    varieta: "Zucchino Romanesco",
+    peso: "250gr",
+    lunghezza: "5cm",
+  },
+  {
+    varieta: "Zucchino",
+    peso: "",
+    lunghezza: "",
+  },
+  {
+    varieta: "",
+    peso: "",
+    lunghezza: "",
+  },
+  {
+    varieta: "",
+    peso: "",
+    lunghezza: "",
+  },
+  {
+    varieta: "",
+    peso: "",
+    lunghezza: "",
+  },
+  {
+    varieta: "",
+    peso: "",
+    lunghezza: "",
+  },
+  {
+    varieta: "",
+    peso: "",
+    lunghezza: "",
+  },
+  {
+    varieta: "",
+    peso: "",
+    lunghezza: "",
+  },
+  {
+    varieta: "",
+    peso: "",
+    lunghezza: "",
+  },
+];
